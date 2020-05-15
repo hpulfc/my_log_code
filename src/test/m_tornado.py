51d74e145eb762710520f4dd8ec7e3244801c29f
hpulfc <hpulfc@yeah.net>
2020-05-15 17:43:51 +0800

    文档添加
    增加tornado相关文档
    
    no-url

---
+++ src/test/m_tornado.py
@@ -0,0 +1,39 @@
+# coding=utf8
+# Time    : 2020/5/15 10:51
+# File    : tornado.py
+# Software: PyCharm
+import functools
+import tornado
+from tornado import gen
+from tornado.httpclient import AsyncHTTPClient
+
+
+async def async_req(url):
+    client = AsyncHTTPClient()
+    resp = await client.fetch(url)
+    return resp.body
+
+
+urls = ["https://www.baidu.com", "https://www.sina.com"]
+
+await_ables = map(lambda _: functools.partial(async_req, _), urls)
+
+for item in await_ables:
+    print(item)
+
+wait_iterator = gen.WaitIterator(await_ables)
+
+
+def item_wait():
+    while not wait_iterator.done():
+        try:
+            result = yield wait_iterator.next()
+        except Exception as e:
+            print(wait_iterator.current_index, e)
+        else:
+            print("Result {} received from {} at {}".format(
+                result, wait_iterator.current_future,
+                wait_iterator.current_index))
+
+
+item_wait()
---
+++ src/test/tt.py
@@ -0,0 +1,6 @@
+# coding=utf8
+# Time    : 2020/5/15 15:29
+# File    : tt.py
+# Software: PyCharm
+
+import tornado
